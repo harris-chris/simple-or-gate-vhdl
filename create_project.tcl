39181b9642ee6a967f6dccbfe0f9b4a3d89d462d
harris-chris <chrisharriscjh@gmail.com>
2021-10-25 09:37:46 +0900

    First commit

--- create_project.tcl
+++
@@ -1,4 +0,0 @@
-create_project -force -part xc7a35ticsg324-1L thermostat
-add_files -fileset sources_1 src/rtl
-set_property file_type {VHDL 2008} [get_files -of_objects [get_filesets sources_1]]
-exit
